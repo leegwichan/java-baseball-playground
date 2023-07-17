62b208faeace0fc21ea739bb16ab6a2177362580
leegwichan <cnddkscndgus@naver.com>
2023-07-17 14:58:53 +0900

    feat (StringTest) : String의 substring() 학습 테스트 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringTest {
     @Test
@@ -49,6 +50,35 @@ public class StringTest {
             String actual = string.substring(1, string.length()-1);
 
             assertThat(actual).isEqualTo(expected);
+        }
+    }
+
+    @Nested
+    @DisplayName("charAt() 테스트")
+    class CharAtTest {
+
+        @Test
+        @DisplayName("문자가 주어졌을 때 0번쨰 값을 가져올 수 있다")
+        void getFirstCharacterTest() {
+            char expected = 'a';
+
+            char actual = "abc".charAt(0);
+
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("문자의 길이보다 큰 값의 인덱스를 StringIndexOutOfBoundsException이 발생한다")
+        void biggerIndexThanStringSizeTest() {
+            assertThrows(StringIndexOutOfBoundsException.class,
+                    () -> "abc".charAt(3));
+        }
+
+        @Test
+        @DisplayName("음수 인덱스를 넣을 때 StringIndexOutOfBoundsException이 발생한다")
+        void minusIndexTest() {
+            assertThrows(StringIndexOutOfBoundsException.class,
+                    () -> "abc".charAt(-1));
         }
     }
 }
