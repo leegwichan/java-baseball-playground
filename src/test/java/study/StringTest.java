2d2b2a21f79eeecd825a319eb6dd455fdcfcb515
leegwichan <cnddkscndgus@naver.com>
2023-07-17 14:52:17 +0900

    feat (StringTest) : String의 split() 학습 테스트 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,7 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +11,29 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Nested
+    @DisplayName("split() 테스트")
+    class SplitTest {
+        @Test
+        @DisplayName("\'1,2\'를 ,으로 나누면 1와 2를 포함한다")
+        void splitTwoElementsTest() {
+            String[] expected = new String[]{"1", "2"};
+
+            String[] actual = "1,2".split(",");
+
+            assertThat(actual).contains(expected);
+        }
+
+        @Test
+        @DisplayName("\'1\'를 ,으로 split하면 1로 분리된다.")
+        void splitOneElementTest() {
+            String[] expected = new String[]{"1"};
+
+            String[] actual = "1".split(",");
+
+            assertThat(actual).containsExactly(expected);
+        }
     }
 }
