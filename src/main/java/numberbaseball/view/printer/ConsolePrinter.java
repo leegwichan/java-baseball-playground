bd0090799ce27ce8f775980c8d86b3008afcea83
leegwichan <cnddkscndgus@naver.com>
2023-07-21 14:43:28 +0900

    feat (ConsoleReader) : Console 창을 통해 출력하는 기능 구현

---
+++ src/main/java/numberbaseball/view/printer/ConsolePrinter.java
@@ -0,0 +1,9 @@
+package numberbaseball.view.printer;
+
+public class ConsolePrinter implements Printer {
+
+    @Override
+    public void print(String message) {
+        System.out.println(message);
+    }
+}
