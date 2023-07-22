10c4df72e2ccf77c78e65f96a6e441ca5bb36085
leegwichan <cnddkscndgus@naver.com>
2023-07-22 13:53:32 +0900

    fix (ConsolePrinter) : 출력 시 자동으로 줄바꿈 되지 않도록 수정

--- src/main/java/numberbaseball/view/printer/ConsolePrinter.java
+++ src/main/java/numberbaseball/view/printer/ConsolePrinter.java
@@ -4,6 +4,6 @@ public class ConsolePrinter implements Printer {
 
     @Override
     public void print(String message) {
-        System.out.println(message);
+        System.out.print(message);
     }
 }
